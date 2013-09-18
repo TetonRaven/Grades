ddc241cf89884024e0a0da1409d5e3eb4f257292
TetonRaven <chris.anderman@yahoo.com>
2013-09-17 20:28:12 -0600

    Tightened up validation code.

--- grades.js
+++ grades.js
@@ -1,20 +1,35 @@
 if (!localStorage) {
-	alert("Your browser does not support saving changes. Entries will be lost after closing the browser or reloading the page.");
+    alert("Your browser does not support saving changes. Entries will be lost after closing the browser or reloading the page.");
 }
 
-var isValid = true;
-
 // *** Defines an individual test score
-function testScore(studentName, score){
+function testScore(studentName, score, onvalidate){
     var self = this;
 
     // ** Properties
 
     // * Student's name
-    self.studentName = ko.observable(studentName ? studentName : "student name").extend({ validateName: null });
+    self.studentName = ko.observable(studentName ? studentName : "student name");
 
     // * Student's numeric scores (0-100)
-    self.score = ko.observable(score ? score : 0).extend({ validateScore: null });
+    self.score = ko.observable(score ? score : 0);
+
+    // ** Validation
+
+    // * Name not empty
+    self.studentName.invalid = ko.observable();
+    self.studentName.subscribe(function(newValue) {
+        self.studentName.invalid(newValue ? false : true);
+        onvalidate(!self.score.invalid());
+    });
+
+    // * Score between 0-100
+    self.score.invalid = ko.observable();
+    self.score.subscribe(function(newValue) {
+        var numericNewValue = isNaN(newValue) ? -1 : parseInt(newValue);
+        self.score.invalid((isNaN(numericNewValue) || numericNewValue < 0 || numericNewValue > 100) ? true : false);
+        onvalidate(!self.score.invalid());
+    });
 }
 
 // *** Defines a set of test results
@@ -23,12 +38,15 @@ function testResults(){
 
     // ** Properties
 
+    // * Validations
+    self.invalid = ko.observable(false);
+
     // * All test scores in the test
     self.testScores = ko.observableArray();
 
     // * Minimum test score
     self.minimumScore = ko.computed(function() {
-        if (self.testScores().length == 0) return 0;
+        if (self.testScores().length == 0 || self.invalid()) return 0;
 
         var scoreValues = new Array();
         for (var i = 0; i < self.testScores().length; i++) {
@@ -43,7 +61,7 @@ function testResults(){
 
     // * Maximum test score
     self.maximumScore = ko.computed(function() {
-        if (self.testScores().length == 0) return 0;
+        if (self.testScores().length == 0 || self.invalid()) return 0;
 
         var scoreValues = new Array();
         for (var i = 0; i < self.testScores().length; i++) {
@@ -58,7 +76,7 @@ function testResults(){
 
     // * Average test score
     self.averageScore = ko.computed(function() {
-        if (self.testScores().length == 0) return 0;
+        if (self.testScores().length == 0 || self.invalid()) return 0;
 
         var total = 0;
         var scoreCount = 0; // we'll only want to include set testScores in our calculation
@@ -77,16 +95,24 @@ function testResults(){
 
     // * Add a new score to the list
     self.addNewScore = function() {
-        if (!isValid) {
+        if (self.invalid()) {
             return;
         }
 
-        self.testScores.push(new testScore());
+        self.addScore(null, null);
     };
 
     // * Add a score to the list with the specified values
     self.addScore = function(studentName, score) {
-        self.testScores.push(new testScore(studentName, score));
+        // Create score with values and wire in a handler for when score validated
+        var score = new testScore(
+            studentName,
+            score,
+            self.onTestScoreValidation
+        );
+
+        // Add to list
+        self.testScores.push(score);
     };
 
     // * Delete the given score from the list
@@ -100,60 +126,30 @@ function testResults(){
         if (!window.localStorage) return;
 
         // * Validate.
-        // NOTE: We set a flag when a field goes invalid but we need to re-validate all now to know if changes have made everything valid, since we allow a field to remain invalid.
-        if (!isValid) {
-            isValid = true;
+        // NOTE: We set flag when a field goes invalid but we need to re-validate all now to know if changes have made everything valid, since we allow a field to remain invalid.
+        if (self.invalid()) {
+            self.invalid(false);
             for (var i = 0; i < self.testScores().length; i++) {
                 if (self.testScores()[i].studentName.invalid() || self.testScores()[i].score.invalid()) {
-                    isValid = false;
+                    self.invalid(true);
                     break;
                 }
             }
         }
 
-        if (isValid) {
+        if (!self.invalid()) {
             localStorage["testResults"] = ko.toJSON(self);
         }
     }
-}
 
-// *** Validation
+    // ** Event Handlers
 
-// * Ensure value is an integer between 0-100
-ko.extenders.validateScore = function(target){
-    target.invalid = ko.observable();
-
-    function validate(newValue) {
-        var numericNewValue = isNaN(newValue) ? -1 : parseInt(newValue);
-        target.invalid((isNaN(numericNewValue) || numericNewValue < 0 || numericNewValue > 100) ? true : false);
-        if (target.invalid()) {
-            isValid = false;
+    // * When a score is validated (name or score value) and is invalid, set myself to invalid
+    self.onTestScoreValidation = function(valid) {
+        if (!valid) {
+            self.invalid(true);
         }
-    }
-
-    validate(target());
-
-    target.subscribe(validate);
-
-    return target;
-}
-
-// * Ensure value is not empty
-ko.extenders.validateName = function(target){
-    target.invalid = ko.observable();
-
-    function validate(newValue) {
-        target.invalid(newValue ? false : true);
-        if (target.invalid()) {
-            isValid = false;
-        }
-    }
-
-    validate(target());
-
-    target.subscribe(validate);
-
-    return target;
+    };
 }
 
 // *** Wire up knockout
@@ -167,6 +163,7 @@ if (window.localStorage && localStorage["testResults"]) {
         testResultsViewModel.addScore(storedResults.testScores[i].studentName, storedResults.testScores[i].score);
     }
 }
+
 // Default to one new entry, if nothing loaded
 if (testResultsViewModel.testScores().length == 0) {
     testResultsViewModel.addNewScore();
